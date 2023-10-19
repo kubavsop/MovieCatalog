c6204e0fe6429a7dbca28575f7ddf18fc5d7b759
Maksim <mazhur04@gmail.com>
2023-10-19 11:35:17 +0700

    Integrate user login screen with viewModel

---
+++ app/src/main/java/com/example/moviescatalog/presentation/user_login/UserLoginState.kt
@@ -0,0 +1,8 @@
+package com.example.moviescatalog.presentation.user_login
+
+sealed interface UserLoginState {
+    data object Initial : UserLoginState
+    data object Loading : UserLoginState
+    data object Success : UserLoginState
+    data class Error(val msg: String) : UserLoginState
+}
--- app/src/main/java/com/example/moviescatalog/presentation/user_login/UserLoginViewModel.kt
+++ app/src/main/java/com/example/moviescatalog/presentation/user_login/UserLoginViewModel.kt
@@ -1,8 +1,14 @@
 package com.example.moviescatalog.presentation.user_login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.model.LoginRequest
 import com.example.domain.usecase.LoginUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +16,23 @@ class UserLoginViewModel @Inject constructor(
     private val loginUserUseCase: LoginUserUseCase
 ) : ViewModel() {
 
+    private val _state: MutableLiveData<UserLoginState> = MutableLiveData(UserLoginState.Initial)
+    val state: LiveData<UserLoginState> = _state
+
+    fun login(username: String, password: String) {
+        viewModelScope.launch {
+            try {
+                val loginRequest = LoginRequest(
+                    username = username,
+                    password = password
+                )
+                loginUserUseCase(loginRequest)
+                _state.value = UserLoginState.Success
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                _state.value = UserLoginState.Error(msg = e.message.toString())
+            }
+        }
+    }
 }
