2ba4dae03842701d9094e15a56497a823d4feedd
Maksim <mazhur04@gmail.com>
2023-10-26 00:29:17 +0700

    Refactor validation code for brevity

--- domain/src/main/java/com/example/domain/feature_user_auth/usecase/ValidateFirstNameUseCase.kt
+++ domain/src/main/java/com/example/domain/feature_user_auth/usecase/ValidateFirstNameUseCase.kt
@@ -2,9 +2,6 @@ package com.example.domain.feature_user_auth.usecase
 
 class ValidateFirstNameUseCase {
     operator fun invoke(firstName: String): Boolean {
-        if (firstName.length < 2) {
-            return false
-        }
-        return true
+        return firstName.length >= 2
     }
 }
--- domain/src/main/java/com/example/domain/feature_user_auth/usecase/ValidateLoginUseCase.kt
+++ domain/src/main/java/com/example/domain/feature_user_auth/usecase/ValidateLoginUseCase.kt
@@ -1,10 +1,7 @@
 package com.example.domain.feature_user_auth.usecase
 
 class ValidateLoginUseCase {
-    operator fun invoke(firstName: String): Boolean {
-        if (firstName.length < 2) {
-            return false
-        }
-        return true
+    operator fun invoke(login: String): Boolean {
+        return login.length >= 2
     }
 }
