c45a3ab45f14440fe7769a7eb24f0c493ca88347
Maksim <mazhur04@gmail.com>
2023-11-01 16:26:45 +0700

    Fix splash screen

--- app/src/main/java/com/example/moviescatalog/presentation/SplashScreenActivity.kt
+++ app/src/main/java/com/example/moviescatalog/presentation/SplashScreenActivity.kt
@@ -11,5 +11,11 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).post {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
