92bec37c6baf4c2834d30ab2cf8b966ac3ab7ff1
Maksim <mazhur04@gmail.com>
2023-11-01 16:26:31 +0700

    Add activity Added an activity for the splash screen

---
+++ app/src/main/java/com/example/moviescatalog/presentation/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.example.moviescatalog.presentation
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
