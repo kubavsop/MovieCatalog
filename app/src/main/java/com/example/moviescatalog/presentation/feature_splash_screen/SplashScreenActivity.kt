ed5ea773969b908608c820f40209d9a81b91cf6f
Maksim <mazhur04@gmail.com>
2023-11-15 11:24:31 +0700

    Fix splash screen

--- app/src/main/java/com/example/moviescatalog/presentation/feature_splash_screen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/moviescatalog/presentation/feature_splash_screen/SplashScreenActivity.kt
@@ -1,14 +1,10 @@
-package com.example.moviescatalog.presentation.feature_splas_screen
+package com.example.moviescatalog.presentation.feature_splash_screen
 
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.LOGGER
-import com.example.domain.common.usecase.GetTokenUseCase
 import com.example.domain.feature_profile_screen.usecase.GetProfileUseCase
 import com.example.moviescatalog.presentation.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,9 +17,6 @@ class SplashScreenActivity : AppCompatActivity() {
 
     @Inject
     lateinit var getProfileUseCase: GetProfileUseCase
-
-//    @Inject
-//    lateinit var getTokenUseCase: GetTokenUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,22 +45,3 @@ class SplashScreenActivity : AppCompatActivity() {
         const val IS_TOKEN_ALIVE = "token"
     }
 }
-
-//        lifecycleScope.launch {
-//            val hasToken = getTokenUseCase() != null
-//
-//            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-//            intent.putExtra(HAS_TOKEN, hasToken)
-//
-//            startActivity(intent)
-//            finish()
-//        }
-
-//        Handler(Looper.getMainLooper()).post {
-//            val hasToken = getTokenUseCase() != null
-//
-//            val intent = Intent(this, MainActivity::class.java)
-//            intent.putExtra(HAS_TOKEN, hasToken)
-//
-//            startActivity(intent)
-//            finish()}
